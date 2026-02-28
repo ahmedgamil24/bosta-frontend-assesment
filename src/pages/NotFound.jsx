5e8ad3e0b1c6c5a5d09f5059b4793f3dd49f101a
ahmedgamil24 <agemail5050@gmail.com>
2026-02-28 08:53:29 +0200

    update 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,9 @@
 const NotFound = () => {
   return (
     <div className='flex justify-center h-screen items-center'>
-        404 Not Found..
+        <img src="src/assets/images/notfound-page.pngd" alt="404 Not Found.." />
     </div>
   )
 }
 export default NotFound
+        
